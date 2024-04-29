71ec5d4006be5bedfb89d74ceb231e4380d22579
Gabriel Schurr <gabriel.elias2000@outlook.de>
2024-04-29 23:35:44 +0200

    Update syntheisze.py

--- datasynth/get_youtube_videos_by_channel.py
+++ datasynth/get_youtube_videos_by_channel.py
@@ -8,9 +8,7 @@ def main(args):
     outdir = args["outdir"].strip()
     channel_name = args["channel"].strip().lower()
     limit = args.get("limit", None)
-    videos = scrapetube.get_channel(
-        channel_username=channel_name, sleep=0.1, limit=limit
-    )
+    videos = scrapetube.get_channel(channel_username=channel_name, sleep=0.1, limit=limit)
 
     try:
         first_video = videos.__next__()
@@ -27,19 +25,16 @@ def main(args):
 
     with open(outfile, "w") as f:
         f.write(first_video["videoId"] + "\n")
-        for video in videos:
+        for i, video in enumerate(videos, 2):
             f.write(video["videoId"] + "\n")
-
-    print("Done!")
     return
 
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
-    ap.add_argument(
-        "-c", "--channel", required=True, help="Name of the YouTube channel"
-    )
+    ap.add_argument("-c", "--channel", required=True, help="Name of the YouTube channel")
     ap.add_argument("-o", "--outdir", required=True, help="Output Directory")
-    ap.add_argument("-l", "--limit", required=False, help="Limit number of videos")
+    ap.add_argument("-l", "--limit", required=False, help="Limit number of videos", type=int)
     args = vars(ap.parse_args())
     main(args)
+    print("Done")
